ff38e4c28a9764c02d9006bbcba8f3a5863fda80
Gianluca Guarini <gianluca.guarini@gmail.com>
2014-01-24 23:35:53 +0100

    removed: useless stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,16 +16,11 @@ module.exports = function(grunt) {
       all: [
         'Gruntfile.js',
         'tasks/*.js',
-        '<%= nodeunit.tests %>',
+        'tests/*.js',
       ],
       options: {
         jshintrc: '.jshintrc',
       },
-    },
-
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp'],
     },
 
     // Configuration to be run (and then tested).
@@ -43,16 +38,11 @@ module.exports = function(grunt) {
         },
         files: [{
           src:[
-            'test/fixtures/*.js'
+            'tests/*.js'
           ]
         }],
       }
-    },
-
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
-    },
+    }
 
   });
 
@@ -61,11 +51,8 @@ module.exports = function(grunt) {
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'testardo']);
+  grunt.registerTask('test', ['jshint']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'testardo']);
